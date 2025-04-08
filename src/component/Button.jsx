c66e0f1447a77b1e92060c814207e3758047e17e
severogianluca <gianlucasevero300@gmail.com>
2025-04-08 18:32:21 +0200

    Feat: fix bottom

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,14 +1,25 @@
+import languages from "../data/languages";
 function Button() {
+
+
+
     return (
-        <div className="container gap-btn">
-            <button type="button" className="btn btn-primary">Html</button>
-            <button type="button" className="btn btn-primary">Css</button>
-            <button type="button" className="btn btn-primary">Javascript</button>
-            <button type="button" className="btn btn-primary">Node.JS</button>
-            <button type="button" className="btn btn-primary">Express</button>
-            <button type="button" className="btn btn-primary">ReactJS</button>
-        </div>
+        
+        languages.map(element =>(
+
+            <button type="button" className="btn btn-primary">{element.title}</button>
+            
+        ))
+          
     );
 }
 
 export default Button
+
+
+{/* <button type="button" className="btn btn-primary">Html</button>
+<button type="button" className="btn btn-primary">Css</button>
+<button type="button" className="btn btn-primary">Javascript</button>
+<button type="button" className="btn btn-primary">Node.JS</button>
+<button type="button" className="btn btn-primary">Express</button>
+<button type="button" className="btn btn-primary">ReactJS</button> */}
--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -2,9 +2,16 @@ import languages from "../data/languages"
 import Button from "./Button"
 
 console.log(languages)
-function Main(){
-    return(
-        <Button />
+function Main() {
+    return (
+        <>
+            <div className="bg-grey">
+                <h1>Learn Web Development</h1>
+            </div>
+            <Button />
+        </>
+
+
     )
 }
 export default Main
